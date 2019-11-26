d72767684a1445b6eb2b12f8eb38e24e06552f3e
20397 <20397@MININT-RTINT45.columbushs.com>
2019-11-26 08:49:29 -0500

    Done!

--- src/atm/machine/ATMMachine.java
+++ src/atm/machine/ATMMachine.java
@@ -9,13 +9,35 @@ package atm.machine;
  *
  * @author mlarrubia
  */
+import java.util.Scanner;
 public class ATMMachine {
 
     /**
      * @param args the command line arguments
      */
+    static Scanner kb = new Scanner(System.in);
+    private static final String username = "Mr.Poop123";
+    private static final int PIN = 1234;
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Please enter your username: ");
+        String nameOfUser = kb.nextLine();
+        System.out.println("Now enter your PIN: ");
+        int pin = kb.nextInt();
+        
+        if(credentials(nameOfUser, pin))
+        
+    }
+    public static boolean credentials(String nameOfUser, int pin){
+    if (nameOfUser.equals(username) && pin == PIN){
+        System.out.println("Welcome to your Wells Fargo bank accout!");
+    return true;
+      }
+    else{
+    System.out.println("You have been temporarily locked out of your account... Please contact customer support.");
+    return false;
     }
     
+    }
 }
